0c4c27f0e26f77e7739ab32fa317cecf0e7bc4d5
Erick Bontempo <108167326+eckbontempo@users.noreply.github.com>
2022-09-21 15:41:15 -0300

    up ex 14 e 15

---
+++ mundo1/14.py
@@ -0,0 +1,3 @@
+n = float(input('Digite a temperatura'))
+
+print('O valor em Fahrenheit é:{} '.format(n*1.8+32))
---
+++ mundo1/15.py
@@ -0,0 +1,9 @@
+n = float(input('Quantos dias utilizado'))
+y = float(input('Quantos KM percorridos'))
+
+vn = 60 * n
+vy = 0.15 * y
+
+print('O valor total das diarias: \n dias{:3.0f} valor: R${:.2f}'.format(n, vn))
+print('O valor total por KM rodado valor: R${:.2f}'.format(vy))
+print('total: R${}'.format(vy+vn))
